79e6ea6eafcef137f687a6523042faf6bffd5b8f
78HP <78HP@78HP-PC>
2021-03-22 11:48:05 +0100

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,10 +37,13 @@ namespace API
 
             });
             services.AddControllers();
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +59,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
